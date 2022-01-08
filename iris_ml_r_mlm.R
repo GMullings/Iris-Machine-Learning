775fd5662820032535d15278ee0da270b468e2f7
Geoffery Mullings <geoffmullings@gmail.com>
2022-01-08 14:04:35 -0500

    Starting Univariate Plots

--- iris_ml_r_mlm.R
+++ iris_ml_r_mlm.R
@@ -36,3 +36,27 @@ validation <- dataset[-validation_index,]
 # Turning "dataset" into the remaining 80% of data for model training and testing, to make things simple
 
 dataset <- dataset[validation_index,]
+
+# EDA
+
+dim(dataset)
+sapply(dataset, class)
+head(dataset)
+
+# Factor class has multiple labels
+
+levels(dataset$Species) # Multinomial
+
+# Summarizing class & attribute distribution
+
+percentage <- prop.table(table(dataset$Species))*100
+cbind(freq=table(dataset$Species), percentage=percentage)
+summary(dataset)
+
+# Going to graph univariate plots to visualize variable distributions
+# Splitting input and output variable columns
+
+x <- dataset[,1:4]
+y <- dataset[,5]
+
+# Boxplot for each attribute on one image
